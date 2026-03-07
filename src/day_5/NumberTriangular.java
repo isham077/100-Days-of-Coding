df3c9e1801e819230bba8f91438443bfa93c7dab
Isham <150380425+isham077@users.noreply.github.com>
2026-03-07 14:18:58 +0530

    day 5 practice

---
+++ src/day_5/NumberTriangular.java
@@ -0,0 +1,17 @@
+package day_5;
+
+public class NumberTriangular {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i + j > 5) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
