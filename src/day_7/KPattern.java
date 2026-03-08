4e4133f5f4f37def1904d81f0949783804baf726
Isham <150380425+isham077@users.noreply.github.com>
2026-03-09 00:03:28 +0530

    k_Pattern problem done

--- src/day_7/KPattern.java
+++ src/day_7/KPattern.java
@@ -1,4 +1,23 @@
 package day_7;
 
 public class KPattern {
+    public static void main(String args[]) {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int j = 1; j <= 5; j++) {
+
+            for (int k = 1; k <= j; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
 }
