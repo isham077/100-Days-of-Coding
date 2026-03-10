93e9361bee5b72d1ec83584c8433dfa2f1700903
Isham <150380425+isham077@users.noreply.github.com>
2026-03-10 23:14:34 +0530

    RhombusPattern done

---
+++ src/day_8/RhombusPattern.java
@@ -0,0 +1,16 @@
+package day_8;
+
+public class RhombusPattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= (4 + i - 1); j++) {
+                if (j >= i && j <= (4 + i - 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
