56de8aeb0431bc47fec9fb4c2335582bd2b56988
Isham <150380425+isham077@users.noreply.github.com>
2026-03-12 23:30:54 +0530

    Day 10: Functions practice

---
+++ src/day_10/Multiply2Numbers.java
@@ -0,0 +1,18 @@
+package day_10;
+import java.util.*;
+
+// Practicing Functions
+
+public class Multiply2Numbers {
+        //Multiply 2 numbers
+        public static int multiply(int a, int b) {
+            return a*b;
+        }
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = multiply(a, b);
+            System.out.println(result);
+        }
+}
---
+++ src/day_10/SumOf2Numbers.java
@@ -0,0 +1,17 @@
+package day_10;
+
+public class SumOf2Numbers {
+
+    public static void sum(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 89;
+        sum(a, b); // Function Call
+    }
+}
+
+
