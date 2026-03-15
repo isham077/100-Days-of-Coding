4e6ab136c110d49239dc202ce3b793124c0be3a8
Isham <150380425+isham077@users.noreply.github.com>
2026-03-15 07:25:16 +0530

    factorial of a number

---
+++ src/day_11/ButterflyStarPattern.java
@@ -0,0 +1,22 @@
+package day_11;
+
+public class ButterflyStarPattern {
+    public static void main(String[] args) {
+        int n = 4;   // This is the maximum width (the middle row)
+
+        // Part 1: Growing Top Half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+                {
+                    for (int w = n; w >= 0; w--) {
+                        for (int e = 1; e <= 5; e++) {
+                            System.out.print("*");
+                        }
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
