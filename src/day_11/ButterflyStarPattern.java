57fb02c2082d532b5597e9687f89711149a4eacf
Isham <150380425+isham077@users.noreply.github.com>
2026-03-15 07:27:41 +0530

    small changes made

--- src/day_11/ButterflyStarPattern.java
+++ src/day_11/ButterflyStarPattern.java
@@ -2,9 +2,8 @@ package day_11;
 
 public class ButterflyStarPattern {
     public static void main(String[] args) {
-        int n = 4;   // This is the maximum width (the middle row)
+        int n = 4;
 
-        // Part 1: Growing Top Half
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
