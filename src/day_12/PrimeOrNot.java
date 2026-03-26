deaf15b1294d003e22067cb31ac8e467cad16a17
Isham <150380425+isham077@users.noreply.github.com>
2026-03-26 22:09:08 +0530

    Small change in PrimeOrNot

--- src/day_12/PrimeOrNot.java
+++ src/day_12/PrimeOrNot.java
@@ -3,12 +3,27 @@ package day_12;
 import java.util.Scanner;
 
 public class PrimeOrNot {
-    public static void prime(int n) {
-
+    public static boolean checkPrimeNumber(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int n = sc.nextInt();
+
+        if(checkPrimeNumber(n)){
+            System.out.println("Prime Number");
+        }else{
+            System.out.println("Not Prime Number");
+        }
     }
 }
 
