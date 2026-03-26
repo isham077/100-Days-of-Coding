7bb4bab45eb9d8fa11aea210b9432760a21871b4
Isham <150380425+isham077@users.noreply.github.com>
2026-03-26 23:48:25 +0530

    Learning Recursion

---
+++ src/day_14/FactorialWithRecursion.java
@@ -0,0 +1,16 @@
+package day_14;
+
+public class FactorialWithRecursion {
+    public static int factorial(int n) {
+        if (n > 1) {
+            return n * factorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5 is " + factorial(5));
+    }
+}
+
