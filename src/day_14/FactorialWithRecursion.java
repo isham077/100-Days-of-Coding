026a6128dbfdbe6ab81630229410299a9dba9a9e
Isham <150380425+isham077@users.noreply.github.com>
2026-03-26 23:52:07 +0530

    small changes

--- src/day_14/FactorialWithRecursion.java
+++ src/day_14/FactorialWithRecursion.java
@@ -8,9 +8,7 @@ public class FactorialWithRecursion {
             return 1;
         }
     }
-
     public static void main(String[] args) {
         System.out.println("Factorial of 5 is " + factorial(5));
     }
 }
-
