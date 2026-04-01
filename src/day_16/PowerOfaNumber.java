3c74cdfa74537767f5bca0f400b4399177afb667
Isham <ishampk077@gmail.com>
2026-04-01 23:50:51 +0530

    some changes

--- src/day_16/PowerOfaNumber.java
+++ src/day_16/PowerOfaNumber.java
@@ -8,15 +8,16 @@ public  class PowerOfaNumber {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int n = sc.nextInt();
-        powerNumber(x,n);
+        powerNumber(x, n);
     }
 
     public static void powerNumber(int x, int n) {
-        int result = (int) Math.pow(x, n);
-        System.out.println(result);
+
+        int result = 1;
+
+        for (int i = 0; i < n; i++) {
+            result = result * x;
         }
+        System.out.println("x to the power n is : " + result);
     }
-
-
-
-
+}
