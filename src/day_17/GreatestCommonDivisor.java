ada784a3d52da4a130f27eb2096b9828566c2c2a
Isham <ishampk077@gmail.com>
2026-04-03 00:17:46 +0530

    fully changed the logic

--- src/day_17/GreatestCommonDivisor.java
+++ src/day_17/GreatestCommonDivisor.java
@@ -2,37 +2,26 @@ package day_17;
 import java.util.*;
 
 public class GreatestCommonDivisor {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        calculateGCD(a, b);
+    public static int findGCD(int n1, int n2) {
+        while (n1 != n2) {
+            if (n1 > n2) {
+                n1 = n1 - n2;
+            } else {
+                n2 = n2 - n1;
+            }
+        }
+        return n1;
     }
 
-    public static void calculateGCD(int a, int b) {
-        int x = 0;
-        int y = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= a; i++) {
-            if (a % i == 0) {
-                x = i;
-//                System.out.print(i + " ");
-            }
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
 
+        int result = findGCD(n1, n2);
 
-        }
-
-            for (int j = 1; j <= b; j++) {
-                if (b % j == 0) {
-                    y = j;
-//                    System.out.print(j + " ");
-                }
-            }
-
-
-
-        int highest = Math.max(x, y);
-        System.out.println("Highest: " + highest);
+        System.out.println("GCD is: " + result);
     }
 }
 
