82ed66cda313adc26352e704b5c24e6dcef7d7ce
Isham <ishampk077@gmail.com>
2026-04-03 22:27:25 +0530

    Arrar Practice

--- src/day_18/ArrayPractice1.java
+++ src/day_18/ArrayPractice1.java
@@ -1,8 +1,21 @@
 package day_18;
 
-import java.util.Scanner;
-
 public class ArrayPractice1 {
+        public static void main(String []args) {
+            numbersTest();
+        }
+
+        public static void numbersTest(){
+            int [] number = new int[3];
+            number[0] =123 ;
+            number[1] =456 ;
+            number[2] =799 ;
+
+            //print the numbers
+            for(int i=0;i<3;i++){
+                System.out.println(number[i]);
+            }
+        }
 
 }
 
