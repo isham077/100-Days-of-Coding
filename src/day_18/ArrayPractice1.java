68827348edc1050f74a588fcb82b54fece8d6176
Isham <ishampk077@gmail.com>
2026-04-03 22:57:42 +0530

    sum of arrays

--- src/day_18/ArrayPractice1.java
+++ src/day_18/ArrayPractice1.java
@@ -1,21 +1,34 @@
 package day_18;
 
 public class ArrayPractice1 {
-        public static void main(String []args) {
-            numbersTest();
-        }
+    public static void main(String[] args) {
+        numbersTest();
+        sumOfArray();
+    }
 
-        public static void numbersTest(){
-            int [] number = new int[3];
-            number[0] =123 ;
-            number[1] =456 ;
-            number[2] =799 ;
+    public static void numbersTest() {
+        int[] number = new int[3];
+        number[0] = 123;
+        number[1] = 456;
+        number[2] = 789;
 
-            //print the numbers
-            for(int i=0;i<3;i++){
-                System.out.println(number[i]);
-            }
+        //print the numbers
+        for (int i = 0; i < 3; i++) {
+            System.out.println(number[i]);
         }
+    }
+
+    public static void sumOfArray() {
+        int[] numbers2 = {5, 94};
+        numbers2[0] = 6;            //changed the first element from 5 to 6.
+        int sum = 0;
+
+        // Loop through the array and add each element to sum
+        for (int i = 0; i < numbers2.length; i++) {
+            sum += numbers2[i];
+        }
+        System.out.println("The sum is : " + sum);
+    }
 
 }
 
