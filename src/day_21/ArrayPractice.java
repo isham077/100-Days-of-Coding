e119ae2f1784116a92139e21b51bf33999ce04ff
Isham <ishampk077@gmail.com>
2026-04-22 23:23:19 +0530

    array practice

--- src/day_21/ArrayPractice.java
+++ src/day_21/ArrayPractice.java
@@ -1,27 +1,30 @@
 package day_21;
 
 import java.util.*;
-
 public class ArrayPractice {
 
-        public static void main(String args[]) {
-            Scanner sc = new Scanner(System.in);
-            int size = sc.nextInt();
-            int numbers[] = new int[size];
-//input
-            for(int i=0; i<size; i++) {
-                numbers[i] = sc.nextInt();
-            }
-            boolean isAscending = true;
-            for(int i=0; i<numbers.length-1; i++) { // NOTICE numbers.length - 1 as     termination condition
-                if(numbers[i] > numbers[i+1]) { // This is the condition for     descending order
-                    isAscending = false;
-                }
-            }
-            if(isAscending) {
-                System.out.println("The array is sorted in ascending order");
-            } else {
-                System.out.println("The array is not sorted in ascending order");
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+
+        for(int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        boolean isAscending = true;
+
+        for(int i = 0; i < numbers.length - 1; i++) {
+            if(numbers[i] > numbers[i+1]) {
+                isAscending = false;
             }
         }
+
+        if(isAscending) {
+            System.out.println("The array is sorted in ascending order");
+        } else {
+            System.out.println("The array is NOT sorted in ascending order");
+        }
+    }
 }
