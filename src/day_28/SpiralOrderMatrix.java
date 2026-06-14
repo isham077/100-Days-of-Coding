7b2a1fb6d8e0b69446fdb07d693a6188e3651387
Isham <ishampk077@gmail.com>
2026-06-14 22:52:16 +0530

    SumOfMatrix

---
+++ src/day_28/SpiralOrderMatrix.java
@@ -0,0 +1,18 @@
+package day_28;
+
+public class SpiralOrderMatrix {
+    public static void main(String[] args){
+        int [][] numbers = {{1,5,7,9,10,11},{6,10,12,13,20,21},{9,25,29,30,32,41},{15,55,59,63,68,70},{40,70,79,81,95,105}};
+
+        int n=0;
+        int m=0;
+        int row_start = 0;
+        int row_end = n-1;
+        int column_start = 0;
+        int column_end = m-1;
+
+
+
+
+    }
+}
