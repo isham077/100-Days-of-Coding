851af258d1d646ac7cac936f56347b3eb3ac3c57
Isham <ishampk077@gmail.com>
2026-06-28 23:21:51 +0530

    MatrixAddition

--- src/day_38/MatrixAddition.java
+++ src/day_38/MatrixAddition.java
@@ -1,4 +1,47 @@
 package day_38;
-
+import java.util.Scanner;
 public class MatrixAddition {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] matrixA = new int[rows][cols];
+        int[][] matrixB = new int[rows][cols];
+        int[][] result = new int[rows][cols];
+
+        // Input Matrix A
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                matrixA[i][j] = sc.nextInt();
+            }
+        }
+
+        // Input Matrix B
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                matrixB[i][j] = sc.nextInt();
+            }
+        }
+
+        // Matrix Addition
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        // Print Result Matrix
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+
 }
