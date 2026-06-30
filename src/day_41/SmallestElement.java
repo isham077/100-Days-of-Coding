3d25ab8fff699f992db7c6adf2b9833d86fe8404
Isham <ishampk077@gmail.com>
2026-06-30 14:24:39 +0530

    FrequencyMatrix

---
+++ src/day_41/SmallestElement.java
@@ -0,0 +1,40 @@
+package day_41;
+import java.util.Scanner;
+
+public class SmallestElement {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int rows = sc.nextInt();
+            int cols = sc.nextInt();
+
+            int[][] matrix = new int[rows][cols];
+
+            // Input Matrix
+            for(int i = 0; i < rows; i++) {
+                for(int j = 0; j < cols; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            // Assume first element is the smallest
+            int min = matrix[0][0];
+
+            // Find the smallest element
+            for(int i = 0; i < rows; i++) {
+                for(int j = 0; j < cols; j++) {
+
+                    if(matrix[i][j] < min) {
+                        min = matrix[i][j];
+                    }
+
+                }
+            }
+
+            System.out.println("Smallest Element = " + min);
+
+            sc.close();
+        }
+}
+
