54758af50637a92eb683aae74b09c511a57421c1
Isham <ishampk077@gmail.com>
2026-07-01 12:11:46 +0530

    SmallestElement

--- src/day_42/PrintCharacters.java
+++ src/day_42/PrintCharacters.java
@@ -1,7 +1,7 @@
 package day_42;
 import java.util.Scanner;
 
-public class w {
+public class PrintCharacters {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
