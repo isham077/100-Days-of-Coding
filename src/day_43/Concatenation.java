0a8b8cd6d74b18f1af86ebbb2cdfcac6098dc4e9
Isham <ishampk077@gmail.com>
2026-07-03 21:24:25 +0530

    StringsBasics

---
+++ src/day_43/Concatenation.java
@@ -0,0 +1,18 @@
+package day_43;
+
+import java.util.Scanner;
+
+public class Concatenation {
+    public static void main(String[] args) {
+
+        String firstName = "Tony";
+        String secondName = "stark";
+        String fullName = firstName + " " + secondName;
+
+        System.out.println(fullName);
+    }
+}
+
+
+
+
---
+++ src/day_43/LengthofAString.java
@@ -0,0 +1,12 @@
+package day_43;
+
+public class LengthofAString {
+    public static void main(String[] args) {
+
+        String firstName = "Tony";
+        String secondName = "stark";
+        String fullName = firstName + " " + secondName;
+
+        System.out.println(fullName.length());
+    }
+}
