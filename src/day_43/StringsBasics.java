55bdb39b255d5c8eba2d5c27d66a47b5adcad730
Isham <ishampk077@gmail.com>
2026-07-03 21:17:53 +0530

    StringsBasics

---
+++ src/day_43/StringsBasics.java
@@ -0,0 +1,11 @@
+package day_43;
+import java.util.Scanner;
+public class StringsBasics {
+    public static void main(String []args) {
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is : " + name);
+
+    }
+}
