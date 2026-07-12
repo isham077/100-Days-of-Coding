0a99f38b2c28835f1777a1e8f6c3b7e20e885ff8
Isham <ishampk077@gmail.com>
2026-07-12 12:34:55 +0530

    StringPalindrome

---
+++ src/day_48/StrPalindrome.java
@@ -0,0 +1,27 @@
+package day_48;
+import java.util.Scanner;
+public class StrPalindrome {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        int left = 0;
+        int right = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        if(isPalindrome){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
