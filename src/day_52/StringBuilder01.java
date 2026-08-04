84ee67d26528c80073f72890d4e8cc82b4ce3f26
Isham <ishampk077@gmail.com>
2026-08-04 21:47:01 +0530

    StringBuilder01

---
+++ src/day_52/StringBuilder01.java
@@ -0,0 +1,13 @@
+package day_52;
+
+public class StringBuilder01 {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("tony");
+//Insert char
+        sb.insert(0, 'S');
+        System.out.println(sb);
+//delete char
+        sb.delete(0, 1);
+        System.out.println(sb);
+    }
+}
